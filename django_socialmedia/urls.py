5a9fe68413f8872ed64c5307846ac5188bb801d4
shols232 <akinsolaademolatemitope@gmail.com>
2020-08-28 20:29:30 +0100

    added logo and improved base template

--- django_socialmedia/urls.py
+++ django_socialmedia/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django import views
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
