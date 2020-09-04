5febe32ced1f021c774766b0e9a9c8d415b4c156
shols232 <akinsolaademolatemitope@gmail.com>
2020-09-04 12:17:04 +0100

    modify for deploy

--- django_socialmedia/settings.py
+++ django_socialmedia/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -103,7 +104,7 @@ if DEBUG:
     }
 else:
     DATABASES = {
-        'default':config(
+        'default':dj_database_url.config(
             default=config('DATABASE_URL')
         )
     }
