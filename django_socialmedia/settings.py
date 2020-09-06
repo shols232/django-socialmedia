802faed30c662c9e2354ff1bf68a4d5bed7cdf81
shols232 <akinsolaademolatemitope@gmail.com>
2020-09-06 13:13:29 +0100

    modified asgi setting

--- django_socialmedia/settings.py
+++ django_socialmedia/settings.py
@@ -89,7 +89,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(config('REDIS_URL'), 6379)],
+            "hosts": [config('REDIS_URL')],
         },
     },
 }
