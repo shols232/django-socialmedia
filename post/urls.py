ad74b5761716c585fe04be38c3189e3b94acb337
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-09-07 02:02:47 +0100

    createview fix

--- post/urls.py
+++ post/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    # path('test', user_views.ContentCreateListView.as_view(), name = "list"),
-    path('create/', user_views.ContentCreateView.as_view(), name = "create"),
+   # path('create/', user_views.ContentCreateView.as_view(), name = "create"),
     path('', user_views.content_list, name = "content"),
 ]
